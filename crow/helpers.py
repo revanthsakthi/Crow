1aab8340064468b432483de3e85e4a2f0c472ff5
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-03-26 11:28:59 +0100

    id generator fixed
    now it works fine and return a random id every time it's called

--- crow/helpers.py
+++ crow/helpers.py
@@ -1,5 +1,16 @@
 import hashlib
 import random
 
-def generateId(s=random.getrandbits(255)):
-    return hashlib.sha1(s).digest()
+def genID(seed=None):
+    """
+    Generate and return a new sha1 hash.
+    If seed provided, return the sha1 hash of it
+    """
+    #detect if seed provided
+    if seed == None:
+        seed = random.getrandbits(255)
+
+    #hash the thing
+    hasher = hashlib.sha1()
+    hasher.update(str(seed).encode())
+    return hasher.hexdigest()
