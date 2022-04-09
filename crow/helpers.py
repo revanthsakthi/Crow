85480aa1b4e18df622b2a0caf4e48bff6d07327c
Omicron166 <64985125+Omicron166@users.noreply.github.com>
2022-04-09 10:37:09 +0200

    genID int output
    integers are easier to calculate XOR metrics

--- crow/helpers.py
+++ crow/helpers.py
@@ -1,16 +1,34 @@
 import hashlib
 import random
+from sys import byteorder
 
-def genID(seed=None):
+def genID(seed:bytes=None) -> int:
     """
-    Generate and return a new sha1 hash.
-    If seed provided, return the sha1 hash of it
+    Generate and return a new node ID.
+    If seed provided, return it's ID.
+
+    seed must be a bytes object
     """
     #detect if seed provided
     if seed == None:
-        seed = random.getrandbits(255)
+        #seed = random.getrandbits(255
+        #).to_bytes(
+        #    32, #the 255 bytes of random.getrandbits encodes in 32 long bytes object
+        #    byteorder
+        #)
+        
+        seed = random.randbytes(32)
+        #in theory, it's faster
 
     #hash the thing
-    hasher = hashlib.sha1()
-    hasher.update(str(seed).encode())
-    return hasher.hexdigest()
+    hasher = hashlib.sha1(seed)
+    id = int.from_bytes(hasher.digest(), byteorder)
+
+    #fix id lenght
+    if len(str(id)) <= 48:
+        last = random.randint(1,9)
+        id = id + int(
+            str(last) + str('000000000000000000000000000000000000000000000000')
+        )
+    
+    return id
